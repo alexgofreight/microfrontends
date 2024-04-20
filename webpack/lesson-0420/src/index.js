ee0066a1d58f13ec70664d2867c64ba6ab81de66
Alex <alex.wang@gofreight.com>
2024-04-21 03:28:27 +0800

    add css-loader

--- webpack/lesson-0420/src/index.js
+++ webpack/lesson-0420/src/index.js
@@ -1,5 +1,9 @@
 import hello from './hello'
 import addImage from './add-img';
+import HelloButton from './component/hello-button'
 
 hello();
 addImage();
+
+const helloBtn = new HelloButton()
+helloBtn.render()
--- webpack/lesson-0420/webpack.config.js
+++ webpack/lesson-0420/webpack.config.js
@@ -23,6 +23,9 @@ module.exports = {
       }, {
         test: /\.(txt)$/,
         type: 'asset/source',
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
